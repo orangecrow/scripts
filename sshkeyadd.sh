af291aa796c9fc27d8c21075ba24bf49b61cadb6
orangecrow <wynalazca47@gmail.com>
2020-11-11 23:56:54 +0100

    śro, 11 lis 2020, 23:56:54 CET

--- sshkeyadd.sh
+++ sshkeyadd.sh
@@ -3,6 +3,7 @@ ssh-keygen -t rsa -b 4096 -C "wynalazca47@gmial.com" << EOF
 password
 password
 EOF
+#to change password: ssh-keygen -p -f ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 ssh-add id_rsa
 xclip -selection clipboard < id_rsa.pub
