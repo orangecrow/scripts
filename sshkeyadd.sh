5ab691aca95d9b48c8454436558533fcc59297a6
orangecrow <wynalazca47@gmail.com>
2020-11-11 18:37:31 +0100

    Wed Nov 11 18:37:31 CET 2020

--- sshkeyadd.sh
+++ sshkeyadd.sh
@@ -1,10 +1,8 @@
-ssh-keygen -t ed25519 -C "wynalazca47@gmial.com"
-cd .ssh
 ssh-keygen -t rsa -b 4096 -C "wynalazca47@gmial.com" << EOF
-git-key
+
 password
 password
 EOF
 eval "$(ssh-agent -s)"
-ssh-add git-key
-xclip -selection clipboard < git-key.pub
+ssh-add id_rsa
+xclip -selection clipboard < id_rsa.pub
