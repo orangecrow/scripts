58a74dd5dc7e2e20bdb9e04c58ebf34f20bd77d3
orangecrow <wynalazca47@gmail.com>
2020-11-13 16:37:41 +0000

    pią, 13 lis 2020, 16:37:41 UTC

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ usermod -aG wheel adam
 
 #login as adam
 
-pacman -S sudo xorg bspwm sxhkd dmenu firefox gcc make xclip openssh
+pacman -S sudo xorg bspwm sxhkd dmenu firefox gcc make xclip openssh base-devel
 
 mkdir .config
 mkdir .config/bspwm
